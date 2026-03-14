8be19156c60629c4ab8b2c523bca9a38433a8f17
lucascarnelosi <lucascarnelosi@hotmail.com>
2026-03-13 23:37:29 -0300

    adjusting files for deploy

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+
+const { PrismaClient } = pkg
 
 const adapter = new PrismaBetterSqlite3({
   url: "file:./prisma/dev.db"
